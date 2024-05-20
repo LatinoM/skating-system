6f40c4eb4b548a04c1c913a8d1f6aba7c5b436ee
LatinoM <latinom@cardiff.ac.uk>
2024-05-20 21:13:16 +0100

    added .csv scoresheet reader

--- Main.py
+++ Main.py
@@ -1,9 +1,4 @@
 from NonFinalRound import NonFinalRound
-beginner_waltz_one = NonFinalRound([102,105,115,118,121,124,130,142,145,148,151,153,155,160,163,166,170,173,176], 15)
-beginner_waltz_one.take_marks([105,118,121,124,130,142,145,148,151,153,160,163,166,170,176])
-beginner_waltz_one.take_marks([105,115,121,124,130,142,145,148,151,153,155,160,163,166,176])
-beginner_waltz_one.take_marks([105,115,118,121,124,142,145,148,151,153,160,163,166,173,176])
-print(beginner_waltz_one.get_recalled())
 
 novice_wq_one = NonFinalRound([100, 101,104,106,109,112,114,117,123,125,127,129,133,135,141,143,144,146,149,152,156,159,161,165,168,169,175],20)
 # Waltz
@@ -15,5 +10,57 @@ novice_wq_one.take_marks([100,101,104,106,109,112,123,125,129,133,135,141,144,14
 novice_wq_one.take_marks([100,101,106,109,112,117,123,125,127,129,133,141,144,146,149,152,159,161,165,175])
 novice_wq_one.take_marks([104,106,109,112,114,123,125,127,129,133,141,144,146,149,152,156,159,161,165,175])
 
-print(novice_wq_one.get_recalled())
+novice_wq_two = NonFinalRound(novice_wq_one.get_recalled(),15)
+# Waltz
+novice_wq_two.take_marks([101,104,106,109,112,125,129,133,144,146,149,156,161,165,175])
+novice_wq_two.take_marks([100,104,109,112,123,129,133,144,146,149,152,159,161,165,175])
+novice_wq_two.take_marks([100,106,109,112,123,125,129,133,141,144,146,149,156,165,175])
+# Quickstep
+novice_wq_two.take_marks([101,104,106,109,112,125,129,133,141,144,149,156,161,165,175])
+novice_wq_two.take_marks([100,106,109,112,123,129,133,144,146,149,152,159,161,165,175])
+novice_wq_two.take_marks([100,106,109,112,123,125,129,133,144,149,152,156,159,161,175])
+
+novice_wq_three = NonFinalRound(novice_wq_two.get_recalled(),10)
+# Waltz
+novice_wq_three.take_marks([106,109,125,129,133,144,149,156,161,165])
+novice_wq_three.take_marks([106,109,112,123,129,133,146,149,161,165])
+novice_wq_three.take_marks([100,106,109,112,123,125,129,133,156,165])
+# Quickstep
+novice_wq_three.take_marks([106,109,125,129,133,144,149,156,161,165])
+novice_wq_three.take_marks([106,109,112,129,133,146,149,161,165,175])
+novice_wq_three.take_marks([100,106,109,112,123,125,129,133,156,165])
+
+novice_wq_four = NonFinalRound(novice_wq_three.get_recalled(),6)
+#Waltz
+novice_wq_four.take_marks([106,125,133,149,156,165])
+novice_wq_four.take_marks([106,109,112,129,133,165])
+novice_wq_four.take_marks([106,109,112,129,133,156])
+# Quickstep
+novice_wq_four.take_marks([125,133,149,156,161,165])
+novice_wq_four.take_marks([106,112,133,149,161,165])
+novice_wq_four.take_marks([106,109,129,133,156,165])
+
+novice_wq_finalists = novice_wq_four.get_recalled()
+
+#print(f"The {len(novice_wq_finalists)} finalists for the Novice WQ are:")
+#print(novice_wq_finalists)
+
+b_n_tango_one = NonFinalRound()
+b_n_tango_one.read_csv("./examples/liverpool_bn_tango_one.csv")
+print(b_n_tango_one.get_recalled())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- NonFinalRound.py
+++ NonFinalRound.py
@@ -1,5 +1,7 @@
+import csv
 class NonFinalRound:
-    def __init__(self, numbers:list, min_to_qualify:int):
+    def __init__(self, numbers:list = [], min_to_qualify:int = 0, name:str = "Unnamed Dance"):
+        self.name = name
         self.scoreboard = {num:0 for num in sorted(numbers)}
         self.min_to_qualify = min_to_qualify
 
@@ -13,9 +15,12 @@ class NonFinalRound:
         while(len(recalled)<self.min_to_qualify):
             sb = NonFinalRound._get_remaining(sb, recalled)
             highest_marks = max(sb.values())
+            if highest_marks == 0:
+                break
             for num in sb.keys():
                 if sb[num] == highest_marks:
                     recalled.append(num)
+        print(f"{len(recalled)} couples recalled.")
         return sorted(recalled)
     
     def _get_remaining(scoreboard, recalled_so_far):
@@ -25,5 +30,35 @@ class NonFinalRound:
                 output.pop(num)
         return output
     
+    def read_csv(self, pathname):
+        dances = 0
+        couples = 0
+        marks = dict()
+        with open(pathname, mode='r', encoding="utf-8-sig") as file:
+            csv_file = csv.reader(file)
+            for index,line in enumerate(csv_file):
+                if index == 0:
+                    self.judges = len(line)-1
+                    self.name = line[0]
+                    print(f"Reading {self.name} Scoresheet")
+                elif index == 1:
+                    self.min_to_qualify = int(line[0])
+                    print(f"- need {self.min_to_qualify} to be recalled.")
+                elif index == 2:
+                    couples = int(line[0])
+                    print(f"- currently {couples} couples.")
+                elif index == 3:
+                    dances = int(line[0])
+                    print(f"- {dances} dance{'s' if dances>1 else ''}")
+                elif index - 4 % (couples+1) == 0:
+                    print(f"Processing {line[0]}")
+                else:
+                    if line[0] not in marks.keys():
+                        marks[int(line[0])] = 0
+                    marks[int(line[0])] += line[1:].count("x")
+            self.scoreboard = marks   
+            
+
+
 
     
